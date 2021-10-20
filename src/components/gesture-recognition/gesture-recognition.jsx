d76da88cd44e9544870e5667c42a66cc385a0a4e
Dmitry Novogrudskiy <novogrudskiy.dv@gmail.com>
2021-10-20 20:26:49 +0300

    checkpoint gesture descriptions fix 16

--- src/components/gesture-recognition/gesture-recognition.jsx
+++ src/components/gesture-recognition/gesture-recognition.jsx
@@ -47,80 +47,82 @@ export const GestureRecognition = () => {
 
     const detect = async (net) => {
         // console.log("DETECT")
-      // Check data is available
-      if (
-        typeof webcamRef.current !== "undefined" &&
-        webcamRef.current !== null &&
-        webcamRef.current.video.readyState === 4
-      ) {
-        // Get Video Properties
-        const video = webcamRef.current.video;
-        const videoWidth = video.videoWidth;
-        const videoHeight = video.videoHeight;
+        // Check data is available
+        if (
+            typeof webcamRef.current !== "undefined" &&
+            webcamRef.current !== null &&
+            webcamRef.current.video.readyState === 4
+        ) {
+            // Get Video Properties
+            const video = webcamRef.current.video;
+            const videoWidth = video.videoWidth;
+            const videoHeight = video.videoHeight;
 
-        // Set video width
-        webcamRef.current.video.width = videoWidth;
-        webcamRef.current.video.height = videoHeight;
+            // Set video width
+            webcamRef.current.video.width = videoWidth;
+            webcamRef.current.video.height = videoHeight;
 
-        // Set canvas height and width
-        canvasRef.current.width = videoWidth;
-        canvasRef.current.height = videoHeight;
+            // Set canvas height and width
+            canvasRef.current.width = videoWidth;
+            canvasRef.current.height = videoHeight;
 
-        // Make Detections
-        const hand = await net.estimateHands(video);
+            // Make Detections
+            const hand = await net.estimateHands(video);
 
-        if (hand.length > 0) {
-          const GE = new fp.GestureEstimator([raisedHandGesture, leftHandSwipeLeftPositionGesture, leftHandSwipeRightPositionGesture, rightHandSwipeLeftPositionGesture, rightHandSwipeRightPositionGesture]);
-          const gesture = await GE.estimate(hand[0].landmarks, 8);
-          if (gesture.gestures !== undefined && gesture.gestures.length > 0) {
-            // console.log(gesture.gestures);
+            if (hand.length > 0) {
+                const GE = new fp.GestureEstimator([raisedHandGesture, leftHandSwipeLeftPositionGesture, leftHandSwipeRightPositionGesture, rightHandSwipeLeftPositionGesture, rightHandSwipeRightPositionGesture]);
+                const gesture = await GE.estimate(hand[0].landmarks, 8);
+                if (gesture.gestures !== undefined && gesture.gestures.length > 0) {
+                    // console.log(gesture.gestures);
 
-            const confidence = gesture.gestures.map(
-              (prediction) => prediction.confidence
-            );
-            const maxConfidence = confidence.indexOf(
-              Math.max.apply(null, confidence)
-            );
-            const gestureName = gesture.gestures[maxConfidence].name;
-            console.log(gestureName);
+                    const confidence = gesture.gestures.map(
+                        (prediction) => prediction.confidence
+                    );
+                    const maxConfidence = confidence.indexOf(
+                        Math.max.apply(null, confidence)
+                    );
+                    const gestureName = gesture.gestures[maxConfidence].name;
+                    console.log(gestureName);
 
-            if (gestureName === raisedHand) {
-                dispatch(rotationStop());
-            } else if (gestureName === leftHandSwipeLeftPosition || gestureName === leftHandSwipeRightPosition ||
-                gestureName === rightHandSwipeLeftPosition || gestureName === rightHandSwipeRightPosition) {
-                console.log('swipe')
-                if (!lastGesture) {
-                    console.log('!lastGesture');
-                } else if (lastGesture.gestureName === gestureName) {
-                    console.log('same')
-                } else {
-                    console.log('not same')
-                    if (Date.now() - lastGesture.timestamp <= gestureMoveDefineTimeout) {
-                        if ((gestureName === leftHandSwipeRightPosition
-                            && lastGesture.gestureName === leftHandSwipeLeftPosition)
-                            || (gestureName === rightHandSwipeRightPosition &&
-                                lastGesture.gestureName === rightHandSwipeLeftPosition)
-                        ) {
-                            console.log('rotateRight')
-                            dispatch(rotateRight())
-                        } else if ((gestureName === leftHandSwipeLeftPosition
-                            && lastGesture.gestureName === leftHandSwipeRightPosition )
-                            || (gestureName === rightHandSwipeLeftPosition &&
-                                lastGesture.gestureName === rightHandSwipeRightPosition )
-                        ) { {
-                            console.log('rotateLeft')
-                            dispatch(rotateLeft())
+                    if (gestureName === raisedHand) {
+                        dispatch(rotationStop());
+                    } else if (gestureName === leftHandSwipeLeftPosition || gestureName === leftHandSwipeRightPosition ||
+                        gestureName === rightHandSwipeLeftPosition || gestureName === rightHandSwipeRightPosition) {
+                        console.log('swipe')
+                        if (!lastGesture) {
+                            console.log('!lastGesture');
+                        } else if (lastGesture.gestureName === gestureName) {
+                            console.log('same')
+                        } else {
+                            console.log('not same')
+                            if (Date.now() - lastGesture.timestamp <= gestureMoveDefineTimeout) {
+                                if ((gestureName === leftHandSwipeRightPosition
+                                    && lastGesture.gestureName === leftHandSwipeLeftPosition)
+                                    || (gestureName === rightHandSwipeRightPosition &&
+                                        lastGesture.gestureName === rightHandSwipeLeftPosition)
+                                ) {
+                                    console.log('rotateRight')
+                                    dispatch(rotateRight())
+                                } else if ((gestureName === leftHandSwipeLeftPosition
+                                    && lastGesture.gestureName === leftHandSwipeRightPosition)
+                                    || (gestureName === rightHandSwipeLeftPosition &&
+                                        lastGesture.gestureName === rightHandSwipeRightPosition)
+                                ) {
+                                    {
+                                        console.log('rotateLeft')
+                                        dispatch(rotateLeft())
+                                    }
+                                }
+                            }
                         }
+                        setLastGesture({
+                            gestureName,
+                            timestamp: Date.now(),
+                        })
                     }
                 }
             }
-              setLastGesture({
-                  gestureName,
-                  timestamp: Date.now(),
-              })
-          }
         }
-      }
     };
 
     useEffect(() => console.log(lastGesture), [lastGesture]);
