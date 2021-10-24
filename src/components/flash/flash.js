bad1b7620bbd0ee2358ec71a580e4be33655701e
Dmitry Novogrudskiy <novogrudskiy.dv@gmail.com>
2021-10-24 03:32:42 +0300

    deleted vcc-ui

--- src/components/flash/flash.js
+++ src/components/flash/flash.js
@@ -1,4 +1,4 @@
-import React, {useMemo} from "react";
+import React from "react";
 import "./styles.scss";
 import cn from "classnames";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
--- src/index.js
+++ src/index.js
@@ -3,15 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import { StyleProvider, ThemePicker } from 'vcc-ui';
 
 ReactDOM.render(
   <React.StrictMode>
-      <StyleProvider>
-          <ThemePicker variant="light">
-              <App />
-          </ThemePicker>
-      </StyleProvider>
+      <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
